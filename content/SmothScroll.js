8e6963a822ec325ebdff74fc13e56dc253e21802
it's me <69476263+for-lite-page@users.noreply.github.com>
2024-09-04 20:01:36 +0300

    Add files via upload

---
+++ content/SmothScroll.js
@@ -0,0 +1,54 @@
+//Momentum Scrolling
+const requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    window.oRequestAnimationFrame;
+
+const wrapper = document.querySelector(".container"),
+    target = document.getElementsByTagName("body")[0];
+
+
+let easing = "cubic-bezier(0.23, 1, 0.32, 1)",
+    duration = "1s",
+    lastScrollY = window.scrollY,
+    pos = 0;
+
+const init = () => {
+    target.style.height = wrapper.offsetHeight + "px";
+    target.style.overflow = "auto";
+
+    wrapper.style.transition = "transform " + duration + " " + easing;
+    wrapper.style.position = "fixed";
+    wrapper.style.justifyContent = "center";
+    wrapper.style.top = "0";
+    wrapper.style.left = "0";
+    wrapper.style.width = "100%";
+    wrapper.style.padding = "0";
+    wrapper.style.zIndex = "2";
+    wrapper.style.display = "flex";
+    wrapper.style.backfaceVisibility = "hidden";
+
+    loop();
+};
+
+const onScroll = scrollY => {
+    pos = -(scrollY || document.documentElement.scrollTop);
+    wrapper.style.transform = "translateY(" + pos + "px)";
+};
+
+const loop = () => {
+    let scrollY = window.scrollY;
+
+    if (lastScrollY === scrollY) {
+        requestAnimationFrame(loop);
+        return;
+    } else {
+        lastScrollY = scrollY;
+        onScroll(scrollY);
+        requestAnimationFrame(loop);
+    }
+};
+
+init();
